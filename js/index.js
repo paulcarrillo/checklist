e5777c1f38043156c84e06b60c8ded19dabda063
Paul Carrillo <paulcarrillo05@gmail.com>
2017-02-25 18:35:00 -0700

    Using JS ForEach loops to check inputs in the list

---
+++ js/index.js
@@ -0,0 +1,33 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+  console.log(checkboxes);
+
+  let lastChecked;
+
+  function handleCheck(e) {
+    // Check if they have the shift key down
+    // AND  check that they are checking it,
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+       //go ahead and do what we need
+      // loop over every single checkbox
+      checkboxes.forEach(checkbox => {
+        console.log(checkbox);
+        if (checkbox === this || checkbox === lastChecked) {
+          inBetween = !inBetween;
+          console.log('Starting to check them inbetween!');
+        }
+
+        if (inBetween) {
+          checkbox.checked = true;
+        }
+      });
+    }
+
+
+    lastChecked = this;
+  }
+
+
+
+checkboxes.forEach(checkbox => checkbox.addEventListener(
+  'click', handleCheck));
